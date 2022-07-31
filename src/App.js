396d9a5eb84ab615ee6fc153cca894e5eb8cdfb2
jamesperry-github <crimsonperry.github@gmail.com>
2022-07-31 18:59:19 -0500

    added basic noise storage and playback functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
+import blue from './sounds/blue.mp3';
+import green from './sounds/green.mp3';
+import red from './sounds/red.mp3';
+import wrong from './sounds/wrong.mp3';
+import yellow from './sounds/yellow.mp3';
 
 function App() {
+  const [replayArr, setReplayArr] = useState([]);
+
+  const playAndStoreSound = (file) => {
+    let audio = new Audio(file);
+    audio.play();
+    replayArr.push(audio);
+    //console.log("replay sounds here", replayArr);
+  }
+
+  const replaySounds = (arr, delay) => {
+    //console.log("replay sounds here", replayArr);
+    // initialize all calls right away
+    arr.forEach((el, i) => {
+      setTimeout(() => {
+        // each loop, call passed in function
+        console.log(arr[i]);
+        arr[i].play();
+
+        // stagger the timeout for each loop by the index
+      }, i * delay);
+    })
+  }
+
+  const drum = (file, color) => {
+    return (
+      <div
+        className='instrument'
+        style={{
+          backgroundColor: color
+        }}
+        onClick={() => {
+          playAndStoreSound(file);
+        }}
+      ></div>
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <button
+          className='replay-btn'
+          onClick={() => {
+            replaySounds(replayArr, 1000)
+          }}
+        >Replay</button>
+      </div>
+      {drum(blue, 'blue')}
+      {drum(green, 'green')}
+      {drum(red, 'red')}
     </div>
   );
 }
